25f409a7203747296897e79074dad159744ca95f
Chanchal <chanchalagnihotri1987@gmail.com>
2025-11-10 07:39:04 +0530

    Inital commit

--- src/services/auth.js
+++
@@ -1,8 +0,0 @@
-import { postAsJsonAsync } from "../utils/fetch";
-const AuthService = {
-  login: (data) => {
-    return postAsJsonAsync("/auth/login", data);
-  },
-};
-
-export default AuthService;
